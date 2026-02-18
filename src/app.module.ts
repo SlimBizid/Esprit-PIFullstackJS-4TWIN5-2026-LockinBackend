f7715827d1f4a950b550a4487306847e40c0198a
RamOfFate <ramoffate@gmail.com>
2026-02-18 22:51:53 +0100

    - Added User entity with properties and TypeORM integration. - Created UserModule, UserController, and UserService. - Introduced UserType enum for user roles. - Set up global validation pipe in main application. - Configured PostgreSQL database connection in app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'lockin_admin',
+      password: 'lockin123',
+      database: 'lockin_db',
+      entities: [User],     
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
