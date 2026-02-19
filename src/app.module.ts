36b4178e47172ef52a7c9e37857f779112a9ba33
RamOfFate <ramoffate@gmail.com>
2026-02-19 01:36:06 +0100

    Add @nestjs/config dependency and configure global settings in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'lockin_admin',
-      password: 'lockin123',
-      database: 'lockin_db',
-      entities: [User],     
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
