51bf5e673dc7e80aec1e9fb55ccb31ce3b503255
SlimBizid <slimbizid879@gmail.com>
2026-02-20 23:12:22 +0100

    Added user signup service logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { ConfigModule } from '@nestjs/config';
       synchronize: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  NotFoundException,
+  HttpStatus,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserType } from './enums/user-type.enum';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findUsernameExists(username: string): Promise<Boolean> {
+    const user = await this.userRepository.findOneBy({ username });
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  async CreateUser(
+    username: string,
+    password: string,
+    type: UserType,
+    githubhandle?: string,
+  ) {
+    if (await this.findUsernameExists(username)) {
+      throw new HttpException('Username already exists', HttpStatus.CONFLICT);
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await this.userRepository.save({
+      username,
+      hashedPassword,
+      type,
+      githubhandle,
+    });
+  }
+}
