2640b724f933a74d835ea1e7862cbe3481249246
SlimBizid <slimbizid879@gmail.com>
2026-02-20 23:55:32 +0100

    Implemented signin endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserType } from 'src/user/enums/user-type.enum';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async SignUp(
+    username: string,
+    email: string,
+    password: string,
+    githubHandle?: string,
+  ): Promise<void> {
+    return this.userService.CreateUser(username, email, password, githubHandle);
+  }
+}
