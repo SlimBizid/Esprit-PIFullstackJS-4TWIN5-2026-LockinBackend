40954b2d280f96839ff6db77851b6dcfb69bf595
RamOfFate <ramoffate@gmail.com>
2026-02-22 13:26:29 +0100

    Added filtering to findALl
    Added filtering to findAll:
    - type is based on user role
    - search is based on username

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -43,9 +43,17 @@ export class UserController {
     @Request() req,
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
+    @Query('type') type?: UserType,
+    @Query('search') search?: string,
   ) {
     const requesterRole = req.user.type;
-    return this.userService.findAll(Number(page), Number(limit), requesterRole);
+    return this.userService.findAll(
+      Number(page),
+      Number(limit),
+      requesterRole,
+      type,
+      search,
+    );
   }
 
   @UseGuards(JwtAuthGuard)
