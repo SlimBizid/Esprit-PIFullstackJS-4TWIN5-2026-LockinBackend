59d401a72fc26adf8c3e1292c6d59737b4d89863
zeeyach <zied.bnelhadjsghaier@esprit.tn>
2026-02-25 02:32:40 +0100

    Protect delete and restore endpoints (admin only)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,33 @@
 import { Controller, Delete, Patch, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UseGuards, Request, ForbiddenException } from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport'; 
+
+
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Delete(':id')
-  softDelete(@Param('id') id: string) {
-    return this.userService.softRemove(id);
+  @UseGuards(AuthGuard('jwt'))
+@Delete(':id')
+async deleteUser(@Param('id') id: string, @Request() req) {
+
+  if (req.user.type !== 'ADMIN') {
+    throw new ForbiddenException('Only admin can delete users');
   }
 
-  @Patch('restore/:id')
-  restore(@Param('id') id: string) {
-    return this.userService.restore(id);
+  return this.userService.softRemove(id);
+}
+
+ @UseGuards(AuthGuard('jwt'))
+@Patch('restore/:id')
+async restoreUser(@Param('id') id: string, @Request() req) {
+
+  if (req.user.type !== 'ADMIN') {
+    throw new ForbiddenException('Only admin can restore users');
   }
+
+  return this.userService.restore(id);
+}
 }
