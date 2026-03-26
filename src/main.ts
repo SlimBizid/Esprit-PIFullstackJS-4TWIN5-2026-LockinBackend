9a1303e98c1ead9301a58c460d87e293fb797848
SlimBizid <slimbizid879@gmail.com>
2026-03-26 21:25:09 +0100

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  const frontendOrigin = process.env.FRONTEND_URL;
+  app.enableCors({
+    origin: [frontendOrigin],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
