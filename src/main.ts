4266bfe15943b20b09c009091bee858903bb64ac
RamOfFate <ramoffate@gmail.com>
2026-03-29 05:02:12 +0100

    revert config

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   const frontendOrigin = process.env.FRONTEND_URL;
-  const allowedOrigins = [frontendOrigin].filter(
-    (origin): origin is string => !!origin,
-  );
-
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin) {
-        callback(null, true);
-        return;
-      }
-
-      const isExplicitlyAllowed = allowedOrigins.includes(origin);
-      const isLocalDevOrigin =
-        /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin);
-
-      callback(null, isExplicitlyAllowed || isLocalDevOrigin);
-    },
+    origin: [frontendOrigin],
     credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
