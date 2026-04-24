7ce4110a668bea723cb55fd3644793a785e424e2
SlimBizid <slimbizid879@gmail.com>
2026-04-25 00:08:11 +0100

    fix:env loading fix

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -2,15 +2,19 @@ import { AzureStorageModule } from '@nestjs/azure-storage';
 import { Global, Module } from '@nestjs/common';
 import { ImageStorageService } from './image-storage.service';
 
-process.loadEnvFile?.();
+// i hate doing this but it might not go through prod build so i have to nuke it
+// eslint-disable-next-line @typescript-eslint/require-await
+const storageConfigFactory = async () => ({
+  sasKey: process.env['AZURE_STORAGE_SAS_KEY'] ?? '',
+  accountName: process.env['AZURE_STORAGE_ACCOUNT'] ?? '',
+  containerName: process.env['AZURE_STORAGE_CONTAINER_NAME'] ?? '',
+});
 
 @Global()
 @Module({
   imports: [
-    AzureStorageModule.withConfig({
-      accountName: process.env.AZURE_STORAGE_ACCOUNT ?? '',
-      sasKey: process.env.AZURE_STORAGE_SAS_KEY ?? '',
-      containerName: process.env.AZURE_STORAGE_CONTAINER_NAME ?? '',
+    AzureStorageModule.withConfigAsync({
+      useFactory: storageConfigFactory,
     }),
   ],
   providers: [ImageStorageService],
